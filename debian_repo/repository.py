5f900408399fa1574835756498c54762e3fac478
Burak Şenkuş <burak.senkus@venit.org>
2024-10-08 23:01:57 +0300

    bugfix: handle file removal during update

--- debian_repo/repository.py
+++ debian_repo/repository.py
@@ -12,22 +12,22 @@ stop_threads = Event()
 
 
 class DebianRepository:
-    def __init__(self, config: Dict, repo_dir: str, no_watch = True) -> None:
+    def __init__(self, config: Dict, repo_dir: str, no_watch=True) -> None:
         self.conf = config
         self.dir = repo_dir
         self.no_watch = no_watch
         self.update_mutex = Lock()
         self.update_wait_mutex = Lock()
         self.queued_update_requests = 0
-    
+
     @property
     def port(self):
         return int(self.conf["port"])
-        
+
     @property
     def root_dir(self):
         return os.path.dirname(self.dir)
-        
+
     @property
     def dists(self):
         return self.conf["dists"]
@@ -35,15 +35,15 @@ class DebianRepository:
     @property
     def keyring_dir(self):
         return os.path.join(self.root_dir, "keyring")
-    
+
     @property
     def debian_dir(self):
         return os.path.join(self.dir, "debian")
-    
+
     @property
     def dists_dir(self):
         return os.path.join(self.debian_dir, "dists")
-    
+
     @property
     def gpg_key_ok(self):
         """Checks if GPG key exists."""
@@ -51,12 +51,12 @@ class DebianRepository:
             keys = execute_cmd(f'gpg --list-keys', env={'GNUPGHOME': self.keyring_dir})[0].decode('utf-8')
             return self.conf["email"] in keys
         return False
-    
+
     def create_pool_directories(self):
         for dist in self.dists:
-            dist_pool_path = os.path.join(self.dists_dir, dist, "pool")    
+            dist_pool_path = os.path.join(self.dists_dir, dist, "pool")
             os.makedirs(dist_pool_path, exist_ok=True)
-            
+
     def __generate_release__(self, dist, key_id):
         date = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S +0000")
         md5sums = do_hash("MD5Sum", "md5sum", os.path.join(self.dists_dir, dist))
@@ -74,26 +74,28 @@ SignWith: {key_id}
 {md5sums}
 {sha1sums}
 {sha256sums}"""
-        
+
     def start(self):
         """Starts HTTP server and package pool watching. Generates GPG key automatically if it doesn't exist."""
         self.create_pool_directories()
         os.chdir(self.dir)
-        
+
         if not self.gpg_key_ok:
             self.generate_gpg()
-            
+
         self.__generate_connection_guide__()
         self.update()
 
         server_address = ('', self.port)
-        httpd = ThreadedHTTPServer(server_address, lambda *args, **kwargs: AuthHandler(*args, auth=self.conf["auth"], users=self.conf["users"], **kwargs))
+        httpd = ThreadedHTTPServer(server_address, lambda *args, **kwargs: AuthHandler(*args, auth=self.conf["auth"],
+                                                                                       users=self.conf["users"],
+                                                                                       **kwargs))
         log(f"Serving directory '{self.dir}' on port {self.port} ...")
         try:
             if not self.no_watch:
                 watch_thread = Thread(target=self.watch_pools)
                 watch_thread.start()
-        
+
             httpd.serve_forever()
         except KeyboardInterrupt:
             log(f"Requested termination.")
@@ -105,7 +107,7 @@ SignWith: {key_id}
             stop_threads.set()
             httpd.shutdown()
             httpd.server_close()
-                
+
     def generate_gpg(self):
         """Generates GPS key for signing repository."""
         os.makedirs(self.dir, exist_ok=True)
@@ -123,19 +125,23 @@ SignWith: {key_id}
                 "%no-protection"
             ])
 
-            out, err, rc = execute_cmd(f'gpgconf --kill gpg-agent && echo "{cmd_input}" | gpg --full-gen-key --batch --yes', env={'GNUPGHOME': self.keyring_dir})
-            
+            out, err, rc = execute_cmd(
+                f'gpgconf --kill gpg-agent && echo "{cmd_input}" | gpg --full-gen-key --batch --yes',
+                env={'GNUPGHOME': self.keyring_dir})
+
             if "fail" in err.decode("utf-8"):
                 raise Exception(err.decode("utf-8"))
-            
-            out, err, rc = execute_cmd(f'gpg --armor --export {self.conf["email"]} > {os.path.join(self.dir, "publickey.gpg")}', env={'GNUPGHOME': self.keyring_dir})
+
+            out, err, rc = execute_cmd(
+                f'gpg --armor --export {self.conf["email"]} > {os.path.join(self.dir, "publickey.gpg")}',
+                env={'GNUPGHOME': self.keyring_dir})
 
             if not self.gpg_key_ok:
                 raise Exception("Couldn't generate GPG key!")
 
         else:
             log(f"Already exists. If you want to create a new key, run 'rm -r {self.keyring_dir}'")
-            
+
     def update(self):
         with self.update_wait_mutex:
             if self.queued_update_requests >= 2:
@@ -146,57 +152,71 @@ SignWith: {key_id}
         with self.update_mutex:
             log("Acquired lock.")
             log("Updating repository...")
-            if not self.gpg_key_ok:
-                self.generate_gpg()
+            try:
+                if not self.gpg_key_ok:
+                    self.generate_gpg()
 
-            out, err, rc = execute_cmd(f"gpg --list-keys --with-colons | grep '^pub' | cut -d: -f5", env={'GNUPGHOME': self.keyring_dir})
+                out, err, rc = execute_cmd(f"gpg --list-keys --with-colons | grep '^pub' | cut -d: -f5",
+                                           env={'GNUPGHOME': self.keyring_dir})
 
-            key_id = out.decode("utf-8").strip()
+                key_id = out.decode("utf-8").strip()
 
-            os.chdir(self.debian_dir)
+                os.chdir(self.debian_dir)
 
-            for dist in os.listdir(self.dists_dir):
-                for arch in self.conf["architectures"]:
-                    current_dist = os.path.join(self.dists_dir, dist)
-                    pool_path = os.path.join(current_dist, "pool", "stable", arch)
-                    packages_path = os.path.join(current_dist, "stable", f"binary-{arch}")
-                    os.makedirs(packages_path, exist_ok=True)
-                    os.makedirs(pool_path, exist_ok=True)
+                for dist in os.listdir(self.dists_dir):
+                    for arch in self.conf["architectures"]:
+                        current_dist = os.path.join(self.dists_dir, dist)
+                        pool_path = os.path.join(current_dist, "pool", "stable", arch)
+                        packages_path = os.path.join(current_dist, "stable", f"binary-{arch}")
+                        os.makedirs(packages_path, exist_ok=True)
+                        os.makedirs(pool_path, exist_ok=True)
 
-                    packages_file_path = os.path.join(packages_path, 'Packages')
-                    packages_gz_file_path = os.path.join(packages_path, 'Packages.gz')
+                        packages_file_path = os.path.join(packages_path, 'Packages')
+                        packages_gz_file_path = os.path.join(packages_path, 'Packages.gz')
 
-                    out, err, rc = execute_cmd(f"dpkg-scanpackages -m --arch {arch} {os.path.relpath(pool_path, self.debian_dir)} > {packages_file_path}", env={'GNUPGHOME': self.keyring_dir})
-                    if rc != 0:
-                        log(f"Error while scanning packages: {err.decode('utf-8')}")
+                        out, err, rc = execute_cmd(
+                            f"dpkg-scanpackages -m --arch {arch} {os.path.relpath(pool_path, self.debian_dir)} > {packages_file_path}",
+                            env={'GNUPGHOME': self.keyring_dir})
+                        if rc != 0:
+                            log(f"Error while scanning packages: {err.decode('utf-8')}")
 
-                    out, err, rc = execute_cmd(f"gzip -9 -c {packages_file_path} > {packages_gz_file_path}", env={'GNUPGHOME': self.keyring_dir})
-                    if rc != 0:
-                        log(f"Error while zipping package info: {err.decode('utf-8')}")
+                        out, err, rc = execute_cmd(f"gzip -9 -c {packages_file_path} > {packages_gz_file_path}",
+                                                   env={'GNUPGHOME': self.keyring_dir})
+                        if rc != 0:
+                            log(f"Error while zipping package info: {err.decode('utf-8')}")
 
                 release_file_path = os.path.join(current_dist, "Release")
                 with open(release_file_path, "w") as f:
                     f.write(self.__generate_release__(dist, key_id))
 
-                out, err, rc = execute_cmd(f"gpg -abs -u {key_id} --yes -o {os.path.join(current_dist, 'Release.gpg')} {release_file_path}", env={'GNUPGHOME': self.keyring_dir})
-                out, err, rc = execute_cmd(f"gpg --clearsign -u {key_id} --yes -o {os.path.join(current_dist, 'InRelease')} {release_file_path}", env={'GNUPGHOME': self.keyring_dir})
-            os.chdir(self.dir)
-            log("Repository updated.")
+                    out, err, rc = execute_cmd(
+                        f"gpg -abs -u {key_id} --yes -o {os.path.join(current_dist, 'Release.gpg')} {release_file_path}",
+                        env={'GNUPGHOME': self.keyring_dir})
+                    out, err, rc = execute_cmd(
+                        f"gpg --clearsign -u {key_id} --yes -o {os.path.join(current_dist, 'InRelease')} {release_file_path}",
+                        env={'GNUPGHOME': self.keyring_dir})
+                os.chdir(self.dir)
+                log("Repository updated.")
+            except Exception as e:
+                log(f"Error during repository update: {e}")
+                with self.update_wait_mutex:
+                    self.queued_update_requests -= 1
+                raise e
         with self.update_wait_mutex:
             self.queued_update_requests -= 1
-    
+
     def watch_pools(self):
         """Watch package pool directories for any event (file create, delete, modify, move). Calls EventHandler's functions in case of events."""
         from .watcher import Watcher
-        
+
         pool_paths = []
         for dist in self.dists:
-            pool_paths.append(os.path.join(self.dists_dir, dist, "pool") )
+            pool_paths.append(os.path.join(self.dists_dir, dist, "pool"))
 
         watcher = Watcher(stop_event=stop_threads, onupdate=self.update, directories=pool_paths)
-        
+
         watcher.start()
-        
+
     def create_service(self, config_path):
         log("Creating systemd service...")
         service_file_path = f"/etc/systemd/system/{self.conf['short_name']}.service"
@@ -213,46 +233,46 @@ WantedBy=multi-user.target
 
         with open(service_file_path, "w") as f:
             f.write(service_file_content)
-            
+
         out, err, rc = execute_cmd("systemctl daemon-reload")
         if rc != 0:
             log(f"Error while reloading systemctl services: {err.decode('utf-8')}")
-            
+
         out, err, rc = execute_cmd(f"systemctl enable {self.conf['short_name']}.service")
         if rc != 0:
             log(f"Error while enabling {self.conf['short_name']}.service: {err.decode('utf-8')}")
-            
+
         out, err, rc = execute_cmd(f"systemctl start {self.conf['short_name']}.service")
         if rc != 0:
             log(f"Error while starting {self.conf['short_name']}.service: {err.decode('utf-8')}")
 
         log("Done.")
-        
+
     def remove_service(self):
         service_file_path = f"/etc/systemd/system/{self.conf['short_name']}.service"
-        
+
         if not os.path.exists(service_file_path):
             log(f"Service doesn't exist: {service_file_path}")
             return
-        
+
         log("Removing systemd service...")
-            
+
         out, err, rc = execute_cmd(f"systemctl stop {self.conf['short_name']}.service")
         if rc != 0:
             log(f"Error while starting {self.conf['short_name']}.service: {err.decode('utf-8')}")
-            
+
         out, err, rc = execute_cmd(f"systemctl disable {self.conf['short_name']}.service")
         if rc != 0:
             log(f"Error while enabling {self.conf['short_name']}.service: {err.decode('utf-8')}")
-            
+
         out, err, rc = execute_cmd("systemctl daemon-reload")
         if rc != 0:
             log(f"Error while reloading systemctl services: {err.decode('utf-8')}")
-            
+
         os.remove(service_file_path)
 
         log("Done.")
-        
+
     def __generate_connection_guide__(self):
         guide_file_path = os.path.join(self.root_dir, "CONNECTION_GUIDE.md")
         credentials = ""
@@ -287,5 +307,5 @@ sudo apt update
 
         with open(guide_file_path, "w") as f:
             f.write(guide_content)
-        
+
         log(f"Connection guide created in {guide_file_path}")
